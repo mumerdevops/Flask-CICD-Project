e3835d08b50ddf6e3f16b8c6f80e839b40a6ae4f
Muhammad Umer <146199957+mumerdevops@users.noreply.github.com>
2026-02-01 11:34:25 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
 
+@app.route('/')
+def hello():
+    return "<h1>DevOps Pipeline Successful!</h1><p>My Flask app is running in Docker on EC2.</p>"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
